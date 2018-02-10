b56c484aa32f7010c5aca1330b7297230cbbcb49
Oscar Guevara <yhonguevara@gmail.com>
2018-02-10 14:02:04 -0300

    Trabajando en el picture-card

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,14 +1,17 @@
 let yo = require('yo-yo');
 let layout = require('../layout');
+let picture = require('../picture-card');
 
-let content = yo`
-  <div class="timeline">
-    <div class="row">
-      <div class="col s12 m10 offset-m1 l6 offset-l3">
-        
+let content = (pictures) => {
+  return yo`
+    <div class="timeline">
+      <div class="row">
+        <div class="col s12 m10 offset-m1 l6 offset-l3">
+        ${pictures}
+        </div>
       </div>
     </div>
-  </div>
 `;
 
+}
 module.exports = layout(content);
---
+++ src/picture-card/index.js
@@ -0,0 +1,12 @@
+let yo = require('yo-yo');
+
+module.exports = yo`
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="http://materializecss.com/images/sample-1.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title">Card Title</span>
+    </div>
+  </div>
+`;
